a4c364b89e74ee8166ceb2b6c82d822da8cd361e
notBillJames <charmillion@gmail.com>
2021-12-13 14:43:48 -0700

    add message flashing

--- website_beta/website/auth.py
+++ website_beta/website/auth.py
@@ -1,10 +1,12 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash
 
 auth = Blueprint('auth', __name__)
 
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
+    data = request.form
+    print(data)
     return render_template('login.html')
 
 
@@ -13,6 +15,23 @@ def logout():
     return render_template('home.html')
 
 
-@auth.route('/signup')
+@auth.route('/signup', methods=['GET', 'POST'])
 def signup():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        firstName = request.form.get('firstName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        if len(email) < 4:
+            flash('Email too short', category='error')
+        elif len(firstName) < 2:
+            flash('First name must be 2+ characters', category='error')
+        elif password1 != password2:
+            flash("Passwords don't match.", category='error')
+        elif len(password1) < 7:
+            flash('Password must be at least 7 characters.', category='error')
+        else:
+            flash('Account Created!', category='success')
+
     return render_template('signup.html')
