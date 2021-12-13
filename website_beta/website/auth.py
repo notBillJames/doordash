46a9865f22664049c331a355ab1358d5a2893735
notBillJames <charmillion@gmail.com>
2021-12-13 13:53:57 -0700

    import render_template

--- website_beta/website/auth.py
+++ website_beta/website/auth.py
@@ -1,18 +1,18 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 auth = Blueprint('auth', __name__)
 
 
 @auth.route('/login')
 def login():
-    return '<p>Login</p>'
+    return render_template('login.html')
 
 
 @auth.route('/logout')
 def logout():
-    return '<p>Logout</p>'
+    return render_template('home.html')
 
 
 @auth.route('/signup')
 def signup():
-    return '<p>Sign Up</p>'
+    return render_template('signup.html')
