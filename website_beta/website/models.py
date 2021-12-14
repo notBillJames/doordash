68250c70cda9b131257d702fee80bfe5c5173820
notBillJames <charmillion@gmail.com>
2021-12-13 18:03:00 -0700

    add Dash and Order Models

--- website_beta/website/models.py
+++ website_beta/website/models.py
@@ -16,3 +16,25 @@ class User(db.Model, UserMixin):
     password = db.Column(db.String(150))
     first_name = db.Column(db.String(150))
     notes = db.relationship('Note')
+
+
+class Dash(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    start = db.Column(db.DateTime(timezone=True), default=func.now())
+    end = db.Column(db.DateTime(timezone=True))
+    location = db.Column(db.String(150))
+    promo = db.Column(db.Integer, default=0)
+    total_pay = db.Column(db.Integer)
+    gas_cost = db.Column(db.Integer)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+
+class Order(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    restaurant = db.Column(db.String(200))
+    distance = db.Column(db.Integer)
+    pay = db.Column(db.Integer)
+    accept_time = db.Column(db.DateTime(timezone=True), default=func.now())
+    pickup_time = db.Column(db.DateTime(timezone=True), defualt=func.now())
+    dropoff_time = db.Column(db.DateTime(timezone=True), default=func.now())
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
