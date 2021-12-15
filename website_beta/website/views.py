aef877b00edcd378563877392bdecc1eea871bba
notBillJames <charmillion@gmail.com>
2021-12-15 10:32:44 -0700

    setup initial order logging

--- website_beta/website/views.py
+++ website_beta/website/views.py
@@ -2,9 +2,10 @@ from flask import Blueprint, render_template, flash, request, session, jsonify
 from flask.helpers import url_for
 from flask_login import login_required, current_user
 from werkzeug.utils import redirect
-from .models import Dash, Order, Note
+from .models import Dash, Order, Note, PendingOrder
 from . import db
 from datetime import datetime
+from sqlalchemy.sql import func
 import json
 import functools
 
@@ -16,6 +17,7 @@ def session_started(view):
     @functools.wraps(view)
     def wrapped_view(*args, **kwargs):
         if session['dashStart'] is None:
+            flash('Start a Dash Session before ordering', category='error')
             return redirect(url_for('views.start_dashing'))
 
         return view(*args, **kwargs)
@@ -43,27 +45,64 @@ def home():
     return render_template('home.html', user=current_user)
 
 
-@views.route('/start-dashing', methods=['GET', 'POST'])
+@views.route('/dash', methods=['GET', 'POST'])
 @login_required
 def start_dashing():
     if request.method == 'POST':
-        session['dashLocation'] = request.form.get('location')
-        session['dashPromo'] = request.form.get('promo')
-        session['dashStart'] = datetime.strftime(
-            datetime.now(),
-            '%Y-%m-%d %H:%M%S'
+        dashLocation = request.form.get('location')
+        dashPromo = request.form.get('promo')
+
+        dash = Dash(
+            start=func.now(),
+            location=dashLocation,
+            promo=dashPromo
         )
+        db.session.add(dash)
+        db.session.commit()
+
+        session['dashID'] = Dash.query.order_by(Dash.start.desc()).first().id
 
         return redirect(url_for('views.log_order'))
 
     return render_template('start_dashing.html', user=current_user)
 
 
-@views.route('/log-order', methods=['GET', 'POST'])
+@views.route('/order', methods=['GET', 'POST'])
 @login_required
 @session_started
 def log_order():
+    if request.method == 'POST':
+        if request.form['orderReceive']:
+            restaurant = request.form.get('restaurant')
+            destination = request.form.get('destination')
+            distance = request.form.get('distance')
+            pay = request.form.get('pay')
+            order = PendingOrder(
+                restaurant=restaurant,
+                destination=destination,
+                distance=distance,
+                pay=pay,
+                accept_time=func.now()
+            )
+
+            db.session.add(order)
+            db.session.commit()
+
+            obj = PendingOrder.query.order_by(Dash.start.desc()).first()
+            session['orderID'] = obj.id
+
+            return redirect(url_for('views.pickup_order'))
+
     return render_template('log_order.html', user=current_user)
+
+
+'''
+@views.route('/pickup-order', methods=['GET', 'POST'])
+@login_required
+@session_started
+def pickup_order():
+    return render_template('pickup_order.html')
+'''
 
 
 @views.route('/delete-note', methods=['POST'])
